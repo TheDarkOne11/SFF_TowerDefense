e132f054696484c5f67ba9b04b4ea98ead056ab0
SgtPetrov <Sgt.Petrov@gmail.com>
2014-07-03 15:26:46 +0200

    Basic stats for enemies.

---
+++ src/lib/EnemyVar.java
@@ -0,0 +1,14 @@
+package lib;
+
+import core.Screen;
+
+public class EnemyVar {
+	public static int basicReward = 5;
+	public static int basicHealth = 2;
+	public static int basicDamage = 10;
+	public static double basicSpeed = Screen.gridSize;
+	public static double basicAttackSpeed = 4;
+	
+	public static int slimeId = 0;
+	
+}
